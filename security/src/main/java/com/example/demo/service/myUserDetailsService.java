07947ce8e568f3c7e90628fca342a7c02ccaaea4
chaithup79 <chaitanya.parameshwara@gmail.com>
2022-08-25 18:22:52 +0530

    spring security

--- security/src/main/java/com/example/demo/service/myUserDetailsService.java
+++ security/src/main/java/com/example/demo/service/myUserDetailsService.java
@@ -3,9 +3,12 @@ package com.example.demo.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.models.user;
@@ -25,4 +28,8 @@ public class myUserDetailsService implements UserDetailsService {
 		return user.map(myUserDetails::new).get();
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
--- security/src/main/java/com/example/demo/service/securityConfigurer.java
+++ security/src/main/java/com/example/demo/service/securityConfigurer.java
@@ -2,20 +2,23 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class securityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private myUserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,16 +26,30 @@ public class securityConfigurer extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService);
 	}
 
+//	@Bean
+//	public UserDetailsService configAuthentication()
+//	{
+//	
+//		return new UserDetailsService(userDetailsService);
+//	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.authorizeRequests().antMatchers("/api/helloUser").hasAnyRole("USER","ADMIN").antMatchers("/api/helloAdmin")
 				.hasRole("ADMIN").antMatchers("/api/user").permitAll().and().formLogin();
 	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/api/helloUser").hasAnyRole("USER", "ADMIN")
+//				.antMatchers("/api/helloAdmin").hasRole("ADMIN").antMatchers("/api/user").permitAll().and().formLogin();
+//		return http.build();
+//	}
 
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+//	@Bean
+//	public PasswordEncoder getPasswordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
 
 }
